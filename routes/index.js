7c4ba71d213e3c454faf2d98075f12dd77b1b03a
Yeondam Park <giyen9088@gmail.com>
2017-05-17 23:49:07 -0700

    AuthController added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -25,6 +26,8 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
+
 router.get('/register', userController.registerForm);
 
 // 1. Validate the registration data
@@ -32,7 +35,10 @@ router.get('/register', userController.registerForm);
 // 3. we need to log them in
 router.post('/register', 
   userController.validateRegister,
-  userController.register
+  userController.register,
+  authController.login
 );
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
